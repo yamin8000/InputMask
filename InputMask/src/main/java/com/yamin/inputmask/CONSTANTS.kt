4c6deb0d9d8874c9c84dfa7c0add570ed1359ed5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-01 23:16:14 +0800

    Delete InputMask directory

--- InputMask/src/main/java/com/yamin/inputmask/CONSTANTS.kt
+++
@@ -1,7 +0,0 @@
-package com.yamin.inputmask
-
-object CONSTANTS {
-    
-    const val NUM_SIGN = '#'
-    const val SPACE = ' '
-}
--- InputMask/src/main/java/com/yamin/inputmask/Range.kt
+++
@@ -1,3 +0,0 @@
-package com.yamin.inputmask
-
-class Range(var start : Int = -1, var end : Int = -1)
